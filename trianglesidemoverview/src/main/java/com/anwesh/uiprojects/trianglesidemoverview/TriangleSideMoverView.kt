8257e6752209685421acf9573220177475553fb6
Anwesh43 <anweshthecool0@gmail.com>
2018-08-07 11:04:16 +0530

    adding State class

--- trianglesidemoverview/src/main/java/com/anwesh/uiprojects/trianglesidemoverview/TriangleSideMoverView.kt
+++ trianglesidemoverview/src/main/java/com/anwesh/uiprojects/trianglesidemoverview/TriangleSideMoverView.kt
@@ -26,4 +26,24 @@ class TriangleSideMoverView(ctx : Context) : View(ctx) {
         }
         return true
     }
+
+    data class State(var scale : Float = 0f, var prevScale : Float = 0f, var dir : Float = 0f) {
+
+        fun update(cb : (Float) -> Unit) {
+            scale += 0.05f * this.dir
+            if (Math.abs(this.scale - this.prevScale) > 1) {
+                this.scale = this.prevScale + this.dir
+                this.dir = 0f
+                this.prevScale = this.scale
+                cb(this.prevScale)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            if (this.dir == 0f) {
+                this.dir = 1 - 2 * this.prevScale
+                cb()
+            }
+        }
+    }
 }

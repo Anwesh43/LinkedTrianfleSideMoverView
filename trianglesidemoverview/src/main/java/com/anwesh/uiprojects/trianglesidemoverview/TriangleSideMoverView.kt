848b224dbf84366eb5807385b3dc48da3ccb3dfa
Anwesh43 <anweshthecool0@gmail.com>
2018-08-07 11:01:21 +0530

    adding TriangleSideMoverView class

--- trianglesidemoverview/src/main/java/com/anwesh/uiprojects/trianglesidemoverview/TriangleSideMoverView.kt
+++ trianglesidemoverview/src/main/java/com/anwesh/uiprojects/trianglesidemoverview/TriangleSideMoverView.kt
@@ -3,3 +3,27 @@ package com.anwesh.uiprojects.trianglesidemoverview
 /**
  * Created by anweshmishra on 07/08/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+
+class TriangleSideMoverView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}

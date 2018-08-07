125bb502592a55ca52ccd897fd822de2f137b222
Anwesh43 <anweshthecool0@gmail.com>
2018-08-07 11:28:23 +0530

    adding LinkedTSM class

--- trianglesidemoverview/src/main/java/com/anwesh/uiprojects/trianglesidemoverview/TriangleSideMoverView.kt
+++ trianglesidemoverview/src/main/java/com/anwesh/uiprojects/trianglesidemoverview/TriangleSideMoverView.kt
@@ -142,4 +142,28 @@ class TriangleSideMoverView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LinkedTSM(var i : Int) {
+
+        private var curr : TSMNode = TSMNode(0)
+
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
